1a98d7638aa9e2e78faeaf22d5aea68cfd5bbc11
Matthew Miller <mmiller1537@gmail.com>
2022-07-06 17:39:25 -0400

    Completed exercise 5 and successfully passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+
+    if (firstNum < 0 || secondNum < 0 || typeof firstNum != 'number' || typeof secondNum != 'number') {
+        return 'ERROR';
+    }
+    
+    let lowNum = Math.min(firstNum, secondNum);
+    let highNum = Math.max(firstNum, secondNum);
+
+    for (i = lowNum; i <= highNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
