fdc833ad9fb7f745c08385fa6817fb1f030cabff
Matthew Miller <mmiller1537@gmail.com>
2022-07-06 17:23:30 -0400

    Completed exercise 4 and successfully passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray) {
+    
+    const newArray = myArray;
+    
+    for (let i = 1; i < arguments.length; i++) {
+        const index = newArray.indexOf(arguments[i]);
+        if (index > -1) {
+            newArray.splice(index, 1);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
