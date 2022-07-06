4d0d61ea0eefb8be77997886ac785e640f1e566a
Matthew Miller <mmiller1537@gmail.com>
2022-07-06 17:57:04 -0400

    Completed exercise 7 and successfully passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degF) {
+  let degC = ((degF - 32) * (5 / 9));
+  return parseFloat(degC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(degC) {
+  let degF = ((degC * (9 / 5)) + 32);
+  return parseFloat(degF.toFixed(1));
 };
 
 // Do not edit below this line
